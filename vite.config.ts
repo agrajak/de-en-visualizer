da41223caa6e119d04374331c18a0112730b3068
Suhyun <korbots@gmail.com>
2024-04-21 10:05:49 +0900

    chore: add base public url for gh-pages deployment

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+/** @type {import('vite').UserConfig} */
+// vite.config.js
+export default defineConfig({
+  base: "https://agrajak.github.io/de-en-visusalizer/",
+});
