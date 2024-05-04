24a8cdfd13d159fb159ceeba27c8d05c78eb2dfd
Suhyun <korbots@gmail.com>
2024-05-05 02:10:54 +0900

    feat: support partial editor per group component

--- src/main.tsx
+++ src/main.tsx
@@ -5,116 +5,6 @@ import { produce } from 'immer'
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-class Editor {
-  element: HTMLElement;
-  root: Node | null = null;
-  resetNode() {
-    if (!this.root) return;
-
-    const value = getInnerTextFromNode(this.root);
-    this.element.innerHTML = "";
-    this.element.innerText = value;
-
-    this.element.setAttribute("contenteditable", "true");
-    if (document.activeElement !== this.element) this.element.focus();
-    this.root = null;
-  }
-
-  setRoot(root: Node) {
-    this.element.setAttribute("contenteditable", "false");
-
-    this.element.innerText = "";
-
-
-    const element = renderNode(root, () => {
-      this.setRoot(root);
-    });
-    this.root = root;
-
-    this.element.appendChild(element);
-
-    const innerText = this.getInnerText();
-    const url = new URL(window.location.href);
-    url.searchParams.set("query", innerText);
-
-    history.replaceState(null, "", url.toString());
-  }
-
-  getInnerText() {
-    return this.root ? getInnerTextFromNode(this.root) : this.element.innerText;
-  }
-
-  constructor(element: HTMLElement) {
-    this.element = element;
-    this.element.addEventListener("click", () => {
-      this.resetNode();
-    });
-    this.element.addEventListener("blur", () => {
-      const innerText = this.getInnerText();
-      if (innerText) {
-        const node = constructNode(innerText);
-        editor.setRoot(node);
-      }
-    });
-  }
-}
-
-type NodeChangeEvent = { id: string, encoded: boolean };
-
-function EditorComponent({ initText }: { initText: string }) {
-  const [node, setNode] = React.useState(constructNode(initText));
-
-  return <Render node={node} onNodeChanged={(event) => {
-    setNode(produce(node, draftState => {
-      function searchNode(node: Node) {
-        if (node.id === event.id) {
-          node.encoded = event.encoded
-        }
-
-        if (node.type === 'string') return;
-        node.params?.forEach(param => {
-          if (param.value.type === 'url') {
-            searchNode(param.value);
-          }
-        })
-
-      }
-      searchNode(draftState);
-    }))
-  }}></Render>
-}
-
-const editor = document.querySelector('#editor') as HTMLElement
-const root = ReactDOM.createRoot(editor)
-
-root.render(<EditorComponent initText={editor.innerText}></EditorComponent>)
-
-/* const editor = new Editor(document.querySelector("#editor") as HTMLElement);
- editor.setRoot(constructNode(editor.getInnerText()));
-
-document.addEventListener("paste", (e: ClipboardEvent) => {
-  if (e.clipboardData) {
-    const node = constructNode(e.clipboardData.getData("text/plain"));
-    editor.setRoot(node);
-  }
-});
-
-const params = new URLSearchParams(window.location.search);
-const query = params.get("query");
-
-if (query) {
-  const node = constructNode(query);
-  editor.setRoot(node);
-}
-
-*/
-function isEncoded(value: string) {
-  try {
-    return decodeURIComponent(value) !== value;
-  } catch (e) {
-    return false;
-  }
-}
 type BaseNode = {
   id: string;
   encoded?: boolean;
@@ -130,24 +20,114 @@ type Node =
     content: string;
   } & BaseNode);
 
-function Render({ node, onNodeChanged }: { node: Node, onNodeChanged?: (event: NodeChangeEvent) => void }) {
-  return <span className="group" id={node.id}>
-    <div contentEditable={false}><button className="toggle" onClick={() => {
+type NodeChangeEvent = ToggleEncodeEvent | ContentChangedEvent | UpdateNodeEvent;
+
+type ToggleEncodeEvent = { name: 'encode-toggle', id: string, encoded: boolean };
+type ContentChangedEvent = { name: 'content-changed', id: string, content: string };
+type UpdateNodeEvent = { name: 'update-node', id: string, node: Node };
+
+
+function EditorComponent({ initText }: { initText: string }) {
+  const [node, setNode] = React.useState(constructNode(initText));
+  console.log({ node })
+
+  React.useEffect(() => {
+    document.addEventListener("paste", (e: ClipboardEvent) => {
+      if (e.clipboardData) {
+        const text = e.clipboardData.getData("text/plain")
+        setNode(constructNode(text));
+      }
+    });
+  }, []);
+
+  return <Group node={node} onNodeChanged={(event) => {
+    setNode((node) => {
+      function mapNode(node: Node): Node {
+        // terminal condition for recursive function
+        if (node.id === event.id) {
+          console.log({ event, node });
+          if (event.name === 'encode-toggle') {
+            return produce(node, draft => { draft.encoded = event.encoded });
+          }
+          if (event.name === 'content-changed') {
+            return constructNode(event.content);
+          }
+        }
+        // do the search
+        if (node.type === 'string') return node;
+
+        return produce(node, draft => {
+          draft.params?.forEach(param => {
+            param.value = mapNode(param.value);
+          });
+        })
+      }
+      return mapNode(node);
+    });
+  }}></Group>
+}
+
+const params = new URLSearchParams(window.location.search);
+const query = params.get("query");
+
+const editor = document.querySelector('#editor') as HTMLElement
+const root = ReactDOM.createRoot(editor)
+
+root.render(<EditorComponent initText={query ?? editor.innerText}></EditorComponent>)
+
+function isEncoded(value: string) {
+  try {
+    return decodeURIComponent(value) !== value;
+  } catch (e) {
+    return false;
+  }
+}
+
+function Group({ node, onNodeChanged }: { node: Node, onNodeChanged?: (event: NodeChangeEvent) => void }) {
+
+  const [editable, setEditable] = React.useState(false);
+  console.log('group', node.id, node.content, node)
+
+  if (editable) {
+    return (<span className="group" onBlur={(event) => {
+      if (!onNodeChanged) return;
+      const element = event.target;
+      if (!(element instanceof HTMLElement)) return;
+
+      const innerText = element.innerText;
+      onNodeChanged({
+        name: 'content-changed',
+        id: node.id,
+        content: innerText
+      })
+      setEditable(false);
+    }} id={node.id}><div contentEditable>{getInnerTextFromNode((node))}</div></span>)
+
+  }
+  return <span className="group" id={node.id} onClick={event => {
+    if (event.target instanceof HTMLSpanElement) {
+      setEditable(true);
+
+    }
+  }}>
+    <div className="button-group"><button className={node.encoded ? '' : 'invert'} onClick={() => {
       if (!onNodeChanged) return;
       onNodeChanged({
+        name: 'encode-toggle',
         id: node.id,
         encoded: !node.encoded
       })
-    }}>{node.encoded ? 'decode' : 'encode'}</button></div>
+    }}>{node.encoded ? 'decode' : 'encode'}</button>
+
+    </div>
     {node.encoded ? <span className="encoded">{getInnerTextFromNode(node)}</span> : <>
       <span className="content">{node.content}</span>
       {node.type === 'url' ? node.params?.map((param, index) => <React.Fragment key={param.key}>
         <span className="symbol">{index === 0 ? '?' : '&'}</span>
-
         <span>
           <span className="key">{param.key}</span>
           <span className="symbol">=</span>
-          {param.value.type === 'string' ? <span className="value">{param.value.content}</span> : <span className="value-box"><Render node={param.value} onNodeChanged={onNodeChanged}></Render></span>}
+          {param.value.type === 'string' ? <span className="value">{param.value.content}</span> : <span className="value-box"><Group node={param.value} onNodeChanged={onNodeChanged}></Group></span>}
         </span></React.Fragment>) : null}
     </>
     }
@@ -185,7 +165,7 @@ function constructNode(value: string): Node {
       type: "url",
       encoded: isEncoded(value),
       id,
-      content: url.search ? value.split("?")[0] : value,
+      content: url.origin,
       params: Array.from(url.searchParams.entries()).map(([key, value]) => {
         return {
           key,
